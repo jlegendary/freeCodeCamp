8884534c505964a8596be5968dfba307c7657bd5
jlegendary <j.legendary2@gmail.com>
2015-09-09 21:28:21 -0400

    Added 008 - confirmEnding - .js

---
+++ 001-Basic Algorithm Scripting/008-confirmEnding/confirmEnding.js
@@ -0,0 +1,14 @@
+function end(str, target) {
+  var stringLength = str.length;
+  var targetLength = target.length;
+  var differenceLength = stringLength-targetLength;
+  
+  if(str.substr(difLength) == target){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+end('Bastian', 'n');
+
